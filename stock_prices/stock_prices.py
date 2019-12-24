a0ca17c656dd7c0baaa29f47f377b26da9acc064
Levi-Huynh <Levi_huynh@yahoo.com>
2019-12-24 13:56:49 -0800

    stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,9 +2,38 @@
 
 import argparse
 
-def find_max_profit(prices):
-  pass
+"""
+1) INPUT: [] list (stock prices)
+2) output: Max profit integer (max diff from two list items, buy always must subtract an item that come before it in list )
+3)  keep track of curr_minbuy & curr_maxsell 
 
+
+[1050, 270, 1540, 3800, 2]
+
+-loop through use curr_buy - curr_sell => store in result => find max number in result list = return max 
+"""
+
+
+def find_max_profit(prices):
+    results = []
+    for i in range(0, len(prices)):
+
+        curr_buy = prices[i]
+        print("current buy:", curr_buy)
+        for j in range(0, len(prices)):
+            if i > j:
+                curr_sell = prices[j]
+                print("curr sell:", curr_sell, "curr buy:", curr_buy)
+                results.append(curr_buy-curr_sell)
+                print("results:", results)
+    print(results)
+    maxprofit = max(results)
+    return maxprofit
+
+
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
+
+"""
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
@@ -13,3 +42,4 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+  """
