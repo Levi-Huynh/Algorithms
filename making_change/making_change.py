e443b83adfd1c840f1198fdecf3e4127ebded76a
Levi-Huynh <Levi_huynh@yahoo.com>
2020-02-18 15:42:30 -0800

    making change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -15,22 +15,39 @@ We can use a single dime
 
 """
 
-cache ={0:1, 10:4}
+
+cache = {0: 1, 10: 4}
+
+"""
+def helper(denominations):
+    global m
+    m = len(denominations)
+"""
 
 
-def making_change(amount, denominations):
-  if amount <= 0:
-    return 0
-  if denominations == 0:
-    return 0
+def making_change(amount, denominations, lenDenom):
+    if amount == 0:
+        return 1
+    if amount < 0:
+        return 0
+    if (lenDenom <= 0 and amount >= 0):
+        return 0
 
+    return making_change(amount, denominations, lenDenom-1) + making_change((amount-denominations[lenDenom-1]), denominations, lenDenom)
+
+
+denominations = [1, 5, 10, 25, 50]
+lenDenom = len(denominations)
+
+print(making_change(10, denominations, lenDenom))
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
